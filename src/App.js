711a4055388314a24a0f0c0337ac7085becee9d3
zdn2442 <zdn.2442.ok@gmail.com>
2022-07-27 14:04:34 +0700

    belajar component, JSX, export, import, style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Header from "./Component/header";
+import React  from "react";
+import { Input, Button } from "./Component/named";
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p className="tulisan">
+//           Loading...
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Hello World
+//         </a>
+       
+//         <p className="tulisan 3">------------------------:v-------------------</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// function Contoh() {
+//   return (
+//     <div>
+//       <h1>Hello Another World!</h1>
+//     </div>
+//   )
+// }
+
+// export default Contoh
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <React.Fragment>
+      <h1>Latihan Export Import</h1>
+      <Header/>
+      <Input/>
+      <Button/>
+      
+    </React.Fragment>
+    
+      
+  )
 }
+
+//JSX harus dibungkus dalam satu element parent
 
 export default App;
---
+++ src/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page() {
+    return(
+        <React.Fragment>
+            <h1>ini Adalah Page</h1>
+        </React.Fragment>
+    )
+}
+
+export default Page
