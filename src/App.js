947664d8934127972bd2aa0a82e7584c611320f6
zdn2442 <zdn.2442.ok@gmail.com>
2022-08-11 13:33:16 +0700

    main modification

--- src/App.js
+++ src/App.js
@@ -1,45 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
-import Header from "./Component/header";
 import React  from "react";
-import { Input, Button } from "./Component/named";
 
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p className="tulisan">
-//           Loading...
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Hello World
-//         </a>
-       
-//         <p className="tulisan 3">------------------------:v-------------------</p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// function Contoh() {
-//   return (
-//     <div>
-//       <h1>Hello Another World!</h1>
-//     </div>
-//   )
-// }
-
-// export default Contoh
 
 function App() {
   return(
