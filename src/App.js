623c3d5e74b87900f3d450f38548986afae031e3
zdn2442 <zdn.2442.ok@gmail.com>
2022-08-13 11:19:55 +0700

    ujian-commit

--- src/App.js
+++ src/App.js
@@ -1,209 +1,149 @@
-import React  from "react";
-import "./style/style.css"
-import Button from './module/button'
-import Input from './module/input'
-import Card from './module/card'
-import InputLagi from './module/input2';
-import InputJuga from './module/input3';
-import ResetButton from './module/resetButton';
-
+import React from "react";
+import "./style/style.css";
+import Button from "./module/button";
+import Input from "./module/input";
+import Card from "./module/card";
+import InputLagi from "./module/input2";
+import InputJuga from "./module/input3";
+import ResetButton from "./module/resetButton";
 
 export default function App() {
-  const [values, setValues] = React.useState(
-      {
-        username:"",
-        email:"",
-        password:"",
-        confirmPassword:"",
-        ttl:"",
-        gender:""
-      }
-    );
+  const [values, setValues] = React.useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    ttl: "",
+    gender: "",
+  });
 
-      const [data, setData] = React.useState(
-        []
-      )
-      const [errors, setErrors] = React.useState(
-        {}
-      )
+  const [data, setData] = React.useState([]);
+  const [errors, setErrors] = React.useState({});
 
-    const handleChange = (event) => {
-      console.log('jalan gk?');
-      setValues(
-        (values) => {
-          return{
-            ...values,
-            [event.target.name] : event.target.value
-          }
-        }
-      )
-      if (event.target.value !== ""){
-        setErrors(
-          {
-            ...errors,
-            [event.target.value] : true
-          }
-        )
-      }
-    }
-
-    const handleBlur = (e) => {
-      e.preventDefault();
-      if (e.target.value === "") {
-        setErrors(
-        (errors) => {
-          return{
-            ...errors,
-            [e.target.name] : true,
-          }
-        }
-      )
-      }
-      
-    }
-    
-
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      console.log('Form Submit');
-      
-      values.id = new Date().getTimezoneOffset()
-      setData(
-        (data) => {
-          return [...data, values]
-        } 
-      )
-      setValues(() => {
-        return{
-          username:"",
-          email:"",
-          password:"",
-          confirmPassword:"",
-          ttl: "",
-          gender:"",
-        }
+  const handleChange = (event) => {
+    console.log("jalan gk?");
+    setValues((values) => {
+      return {
+        ...values,
+        [event.target.name]: event.target.value,
+      };
+    });
+    if (event.target.value !== "") {
+      setErrors({
+        ...errors,
+        [event.target.value]: true,
       });
     }
+  };
 
-    console.log('error', errors);
+  const handleBlur = (e) => {
+    e.preventDefault();
+    if (e.target.value === "") {
+      setErrors((errors) => {
+        return {
+          ...errors,
+          [e.target.name]: true,
+        };
+      });
+    }
+  };
 
-  return(
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submit");
+
+    values.id = new Date().getTimezoneOffset();
+    setData((data) => {
+      return [...data, values];
+    });
+    setValues(() => {
+      return {
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        ttl: "",
+        gender: "",
+      };
+    });
+    if (e.target.value === "") {
+    alert('This form must be filled')
+    }
+  };
+
+  const handleReset = (e) => {
+    e.preventDefault();
+    setValues(() => {
+      return {
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        ttl: "",
+        gender: "",
+      };
+    });
+  }
+
+  console.log("error", errors);
+
+  return (
     <React.Fragment>
       <div className="container">
         <div className="content satu">
-          <form
-          onSubmit={handleSubmit}
-          >
+          <form onSubmit={handleSubmit}>
             <Input
               isError={errors?.username}
-              textError={'Please fill this correctly'}
+              textError={"Please fill this correctly"}
               name="username"
-              value={values.username} 
-              label={'Username'} 
+              value={values.username}
+              label={"Username"}
               placeholder="Username"
               onBlur={handleBlur}
-              onChange = {
-                (event) => {
-                  event.preventDefault();
-                  console.log('ok jalan');
-                  console.log(event);
-                  setValues(
-                    (values) => {
-                      return{
-                        ...values,
-                        username: event.target.value,
-                      }
-                    }
-                  )
-                }
-              }
+              onChange={(event) => {
+                event.preventDefault();
+                console.log("ok jalan");
+                console.log(event);
+                setValues((values) => {
+                  return {
+                    ...values,
+                    username: event.target.value,
+                  };
+                });
+              }}
             />
-            <Input
-              isError={errors?.email}
-              textError={'Please fill this correctly'}
-              name="email"
-              value={values.email}
-              label={'Email'}
-              placeholder="Email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <Input
-              isError={errors?.ttl}
-              textError={'Please fill this correctly'}
-              name="ttl"
-              value={values.ttl}
-              label={'Place of birth'}
-              placeholder="Place of birth"
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <InputLagi
-              isError={errors?.date}
-              textError={'Please fill this correctly'}
-              name="date"
-              value={values.date}
-              label={"Date of birth"}
-              placeholder="Date of birth"
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <InputJuga
-            isError={errors?.gender}
-            textError={'Please fill this correctly'}
-            name="gender"
-            value={values.gender}
-            label={"Gender"}
-            placeholder="Gender"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            />
-            <Input
-              isError={errors?.password}
-              textError={'Please fill this correctly'}
-              name="password"
-              value={values.password} 
-              label={'Password'} 
-              placeholder="Password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
+            <Input isError={errors?.email} textError={"Please fill this correctly"} name="email" value={values.email} label={"Email"} placeholder="Email" onChange={handleChange} onBlur={handleBlur} />
+            <Input isError={errors?.ttl} textError={"Please fill this correctly"} name="ttl" value={values.ttl} label={"Place of birth"} placeholder="Place of birth" onChange={handleChange} onBlur={handleBlur} />
+            <InputLagi isError={errors?.date} textError={"Please fill this correctly"} name="date" value={values.date} label={"Date of birth"} placeholder="Date of birth" onChange={handleChange} onBlur={handleBlur} />
+            <InputJuga isError={errors?.gender} textError={"Please fill this correctly"} name="gender" value={values.gender} label={"Gender"} placeholder="Gender" onChange={handleChange} onBlur={handleBlur} />
+            <Input isError={errors?.password} textError={"Please fill this correctly"} name="password" value={values.password} label={"Password"} placeholder="Password" onChange={handleChange} onBlur={handleBlur} />
             <Input
               isError={errors?.confirmPassword}
-              textError={'Please fill this correctly'}
+              textError={"Please fill this correcztly"}
               name="confirmPassword"
-              value={values.confirmPassword} 
-              label={'Confirm Password'} 
+              value={values.confirmPassword}
+              label={"Confirm Password"}
               placeholder="Confirm Password"
               onChange={handleChange}
               onBlur={handleBlur}
             />
             <div className="button-container">
-            <ResetButton title={'Reset'}/>
-            <Button title={'Save'} />
+              <ResetButton title={"Reset"} onClick={handleReset}/>
+              <Button title={"Save"} onSubmit={handleSubmit}/>
             </div>
+            
           </form>
         </div>
-        <div className='content dua'>
-          <div className="card" style={
-          {
-           
-          }
-        } 
-        >
-          
-          {/* <h2>Confirmation Data</h2> */}
-          <Card data={data} setData={setData}/>
+        <div className="content dua">
+          <div className="card">
+            {/* <h2>Confirmation Data</h2> */}
+            <Card data={data} setData={setData} />
           </div>
         </div>
       </div>
-      
     </React.Fragment>
-  )
+  );
 }
-
-
-
 
 // function App() {
 //   let [count, setCount] = React.useState(0);
@@ -220,13 +160,13 @@ export default function App() {
 //     <React.Fragment>
 //       <h1>Counting from {count}</h1>
 //       <Button onClick={handleTambah} title={"Tambah"} color="blue"/>
-//       <Button 
-//         onClick={handleKurang} 
+//       <Button
+//         onClick={handleKurang}
 //         title={"Kurang"}
 //         disabled={count <=0 ? true : false} />
-//       <Button 
+//       <Button
 //         onClick={handleReset}
-//         title={"Reset"} 
+//         title={"Reset"}
 //         color="white"/>
 //     </React.Fragment>
 //   )
--- src/module/resetButton.jsx
+++ src/module/resetButton.jsx
@@ -1,27 +1,10 @@
 import React from "react";
 
-export default function ResetButton({ title,...props}) {
-    const handleDelete = (e) => {
-        e.preventDefault();
-        // let filter = data.filter(
-        //   (item) => {
-        //     return item.id !== parseFloat(e.target.value);
-        //   }
-        // )
-       
-        // setData(() => {
-        //   return filter;
-        // })
-    return (
-      <React.Fragment>
-        <button
-        onClick={handleDelete}
-         {...props}
-          className="button "
-        >
-          {title}
-        </button>
-      </React.Fragment>
-    );
-  }
-} 
+export default function ResetButton({ title }) {
+  return (
+    <React.Fragment>
+      <input type="button" className="button-reset" />
+      {title}
+    </React.Fragment>
+  );
+}
