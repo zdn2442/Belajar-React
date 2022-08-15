2006acc0405da66a7f9b32d7418170fdebe747bc
zdn2442 <zdn.2442.ok@gmail.com>
2022-08-15 14:01:15 +0700

    remed-commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Card from './Card';
 
 function App() {
   const [count, setCount] = React.useState(0)
-
+  const [warna, setWarna] = React.useState("white")
   return(
     <React.Fragment>
-      <Card count={count} setCount={setCount}/>
+      <Card count={count} setCount={setCount} warna={warna} setWarna={setWarna}/>
     </React.Fragment>
     
       
--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,53 @@
 import React from "react";
+import "./style/style.css";
 
-export default function Card({count, setCount}) {
-    const handleCount = () => {
-        setCount(count + 1)
-    }
-    const handleMinus = () => {
-        setCount(count - 1)
-    }
-    
-    return(
-        <React.Fragment>
-            <p>Ini Card</p>
-            <h3>
-                {
-                    count <= 10 ? "Kurang dari Sepuluh" :  "Lebih dari Sepuluh"
-                }
-            </h3>
-            {count}
-            <button 
-            onClick={handleCount}>Tambah</button>
-            <button onClick={handleMinus}>Kurang</button>
-        </React.Fragment>
-    )
+export default function Card({ count, setCount, warna, setWarna }) {
+  const handleCount = () => {
+    setCount(count + 1);
+  };
+  const handleMinus = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          backgroundColor: warna,
+          width: 300,
+          height: 300,
+        }}
+      >
+        <p>{warna}</p>
+      </div>
+      <button
+        onClick={() => {
+          setWarna("red");
+        }}
+      >
+        Merah
+      </button>
+      <button
+        onClick={() => {
+          setWarna("blue");
+        }}
+      >
+        Biru
+      </button>
+      <button
+        onClick={() => {
+          setWarna("green");
+        }}
+      >
+        Hijau
+      </button>
+      <p>Ini Card</p>
+      <h3>{count <= 10 ? "Kurang dari Sepuluh" : "Lebih dari Sepuluh"}</h3>
+      {count}
+      <button onClick={handleCount}>Tambah</button>
+      <button onClick={handleMinus} disabled={
+        count < 1 && true 
+      }>Kurang</button>
+    </React.Fragment>
+  );
 }
