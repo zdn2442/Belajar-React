54a5452a6ca44849cc82ef23bc3e3159fec036aa
zdn2442 <zdn.2442.ok@gmail.com>
2022-09-05 13:19:56 +0700

    router-commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Routes, Route} from 'react-router-dom'
 import Login from './pages/login'
 import Register from './pages/register';
+import Admin from './pages/admin'
 
 function App() {
   return(
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
-        <Route>
+        <Route path="/admin" element={<Admin/>}>
           
         </Route>
         <Route path="*" element={<Login/>}/>
