13cd5638e6da8e25ca839957ec0a1629f4fbc69d
zdn2442 <zdn.2442.ok@gmail.com>
2022-11-28 13:34:45 +0700

    pas lagi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React  from "react";
+import Login from './module/login';
+import {Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return(
     <React.Fragment>
-      <h1></h1>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+       
+      </Routes>
+      
     </React.Fragment>
     
       
